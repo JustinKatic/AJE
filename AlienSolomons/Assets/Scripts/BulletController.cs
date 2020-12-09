8e41ac570ead5ef5c1b3d0519897f70d5fca5a13
Justin <justinkatic4@gmail.com>
2020-12-10 10:57:01 +1100

    started adding shoot

---
+++ AlienSolomons/Assets/Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls bullet speed, direction, lifetime and damage.
+/// </summary>
+public class BulletController : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        //Move bullet forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    { 
+        if (collision.gameObject.tag == "Wall")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ AlienSolomons/Assets/Scripts/GunController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles the rate of fire and bullet speed of a gun.
+/// </summary>
+public class GunController : MonoBehaviour
+{
+    public bool isFiring;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+    public Transform firePoint;
+
+    void Update()
+    {
+        if (isFiring)
+        {
+            shotCounter -= Time.deltaTime;
+            if (shotCounter <= 0)
+            {
+                GameObject bullet = ObjectPooler.SharedInstance.GetPooledObject("PlayerBullet");
+                shotCounter = timeBetweenShots;
+                bullet.transform.position = firePoint.position;
+                bullet.transform.rotation = firePoint.transform.rotation;
+                bullet.SetActive(true);
+            }
+        }
+
+    }
+}
