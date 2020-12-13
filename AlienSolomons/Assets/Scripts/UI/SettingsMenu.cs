1172d6ad0866c91ff2b10786d0620da72088574a
ElricM <elric.milton@yahoo.com.au>
2020-12-13 13:41:09 +1100

    made test level layout less bad

--- AlienSolomons/Assets/Scripts/UI/SettingsMenu.cs
+++ AlienSolomons/Assets/Scripts/UI/SettingsMenu.cs
@@ -10,4 +10,9 @@ public class SettingsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("Volume", volume);
     }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
 }
