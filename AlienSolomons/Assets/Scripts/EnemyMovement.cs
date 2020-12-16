edba1addafedb1bcfeb51efa97a137035ce6811b
ElricM <elric.milton@yahoo.com.au>
2020-12-16 13:58:00 +1100

    Added enemy NavMesh movement

---
+++ AlienSolomons/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform destination;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        destination = GameObject.FindGameObjectWithTag("Player").transform;
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (navMeshAgent == null)
+        {
+            Debug.LogError(gameObject.name + "does not have a nav mesh agent.");
+        }
+    }
+
+    private void Update()
+    {
+        navMeshAgent.SetDestination(destination.position);
+    }
+}
