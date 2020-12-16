939bf587159e70484da8afb1b57729d7d96488c6
Justin <justinkatic4@gmail.com>
2020-12-17 10:28:01 +1100

    HealthBar and enemy dealing dmg

---
+++ AlienSolomons/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Handles player health.
+/// </summary>
+public class PlayerHealthManager : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+
+    public HealthBar healthBar;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+    }
+    public void HurtPlayer(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void HealPlayer(float heal)
+    {
+        currentHealth += heal;
+        healthBar.SetHealth(currentHealth);
+    }
+}
---
+++ AlienSolomons/Assets/Scripts/PlayerHurt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHurt : MonoBehaviour
+{
+
+    [SerializeField] float _damageEveryX = 1.0f;
+    float _timer;
+    [SerializeField] float _damage;
+
+    PlayerHealthManager _playerHealthManager;
+
+    private void Start()
+    {
+        _playerHealthManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthManager>();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _timer += Time.deltaTime;
+            if (_timer > _damageEveryX)
+            {
+                _playerHealthManager.HurtPlayer(_damage);
+                _timer = 0.0f;
+            }
+        }
+    }
+}
