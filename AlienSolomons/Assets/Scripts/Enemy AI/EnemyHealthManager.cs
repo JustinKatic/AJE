14a798832b382476034a8da45e1949d863fc27c8
ElricM <elric.milton@yahoo.com.au>
2021-01-04 15:49:21 +1100

    Added enemy health bar

--- AlienSolomons/Assets/Scripts/Enemy AI/EnemyHealthManager.cs
+++ AlienSolomons/Assets/Scripts/Enemy AI/EnemyHealthManager.cs
@@ -7,20 +7,22 @@ public class EnemyHealthManager : MonoBehaviour
 {
     [SerializeField] float _maxHealth;
     private float _currentHealth;
-    NavMeshAgent navMeshAgent;
-    PlayerHurt hurtScript;
-    EnemyMovement moveScript;
+    //NavMeshAgent navMeshAgent;
+    //PlayerHurt hurtScript;
+    //EnemyMovement moveScript;
+    public HealthBar healthBar;
 
     private void Start()
     {
-        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
-        hurtScript = gameObject.GetComponent<PlayerHurt>();
-        moveScript = gameObject.GetComponent<EnemyMovement>();
+        //navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        //hurtScript = gameObject.GetComponent<PlayerHurt>();
+        //moveScript = gameObject.GetComponent<EnemyMovement>();
     }
 
     private void OnEnable()
     {
         _currentHealth = _maxHealth;
+        healthBar.SetMaxHealth(_maxHealth);
     }
 
     private void Update()
@@ -33,13 +35,11 @@ public class EnemyHealthManager : MonoBehaviour
     public void HurtEnemy(float damage)
     {
         _currentHealth -= damage;
+        healthBar.SetHealth(_currentHealth);
     }
 
     void Die()
     {
-        navMeshAgent.enabled = false;
-        moveScript.enabled = false;
-        hurtScript.enabled = false;
-        //gameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
 }
