a719cb44fe5041b66584c248ae4b83775386a0e6
ElricM <elric.milton@yahoo.com.au>
2021-01-04 16:14:31 +1100

    Added terraform damage & slow area. Slow not yet working.

---
+++ AlienSolomons/Assets/Scripts/TerraformSlow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TerraformSlow : MonoBehaviour
+{
+    [SerializeField] float speedReduction;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<NavMeshAgent>().speed *= speedReduction;
+        }
+    }
+}
