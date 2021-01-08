c11a7b128011866336c5d8e35b17e9259e586390
Justin <justinkatic4@gmail.com>
2021-01-08 13:01:01 +1100

    rough start to terraformUpgrades

---
+++ AlienSolomons/Assets/Scripts/ActivateTerraform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTerraform : MonoBehaviour
+{
+    private bool _areaAlreadyTerraformed;
+
+    UpgradeManager _upgradeManager;
+
+    private void Start()
+    {
+        _upgradeManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<UpgradeManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (_areaAlreadyTerraformed || TerraformManager.terraformNoOptionSlected)
+            return;
+
+        if (TerraformManager.terraformSlowChosen)
+        {
+            gameObject.GetComponent<TerraformSlow>().enabled = true;
+            SetTerraformNoOptionSlectedTrue();
+            _upgradeManager.DisplayUpgradeScreen();
+        }
+
+        else if (TerraformManager.terraformDamageChosen)
+        {
+            gameObject.GetComponent<TerraformDamage>().enabled = true;
+            SetTerraformNoOptionSlectedTrue();
+            _upgradeManager.DisplayUpgradeScreen();
+        }
+
+        _areaAlreadyTerraformed = true;
+    }
+
+
+
+    private void SetTerraformNoOptionSlectedTrue()
+    {
+        TerraformManager.terraformSlowChosen = false;
+        TerraformManager.terraformDamageChosen = false;
+        TerraformManager.terraformNoOptionSlected = true;
+    }
+}
