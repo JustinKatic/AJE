8f85bf2e04397a64253765877e97c27165674fa9
ElricM <elric.milton@yahoo.com.au>
2021-01-13 12:37:38 +1100

    Rearranged the menu UI

--- AlienSolomons/Assets/Scripts/UI/MenuManager.cs
+++ AlienSolomons/Assets/Scripts/UI/MenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     [SerializeField] GameObject mainMenu;
     [SerializeField] GameObject settingsMenu;
