464ce311afc75fbbf770a2e0416d1cb5f55ae373
Justin <justinkatic4@gmail.com>
2021-01-14 16:06:09 +1100

    sep button to place traps and towers for testing

---
+++ AlienSolomons/Assets/Scripts/PurchaseTower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseTower : MonoBehaviour
+{
+    [SerializeField] Material _towerTexture;
+    [SerializeField] GameObject _purchaseableTower;
+
+    public void BuyTrap()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            if (hit.collider.tag == "Ground")
+            {
+                hit.collider.gameObject.GetComponent<MeshRenderer>().material = _towerTexture;
+            }
+        }
+    }
+
+    public void BuyTower()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            if (hit.collider.tag == "Ground")
+            {
+                Instantiate(_purchaseableTower,
+                    new Vector3(hit.collider.gameObject.transform.position.x,
+                    hit.collider.gameObject.transform.transform.position.y + 1.2f,
+                    hit.collider.gameObject.transform.position.z),
+                    hit.collider.gameObject.transform.rotation);
+            }
+        }
+    }
+}
