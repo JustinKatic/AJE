2528e80bd4c1750c4c297ee7b8d256e8c18461fa
Justin <justinkatic4@gmail.com>
2021-01-14 13:19:21 +1100

    changed tag on ground tiles to ground

---
+++ AlienSolomons/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+
+    RaycastHit hitInfo;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down,out hitInfo))
+        {
+            if(hitInfo.collider.tag == "Ground")
+            {
+                Debug.Log("HitGround");
+            }
+        }
+    }
+
+
+}
