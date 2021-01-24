6f318e49675173ef21383a23d335e4576994b6ca
Justin <justinkatic4@gmail.com>
2021-01-24 18:51:13 +1100

    added necrotic tower

---
+++ AlienSolomons/Assets/Scripts/Towers/NecroticTower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NecroticTower : TowersDefault
+{
+    [SerializeField] FloatVariable PlayerCurrentHealth;
+    [SerializeField] GameEvent PlayerHealthIncreased;
+    protected override void MyCollisions()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, TowerRadius.Value, EnemyLayerMask.Value);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            hitColliders[i].GetComponent<EnemyHealthManager>().HurtEnemy(TowerDamage.Value);
+            PlayerCurrentHealth.Value += TowerDamage.Value / 2;
+            PlayerHealthIncreased.Raise();
+            i++;
+        }
+    }
+}
