09f6c08f274b4e18c867170f1e14d9c57cf96556
Justin <justinkatic4@gmail.com>
2021-01-24 18:30:40 +1100

    made towers into own class that inherit from towers

---
+++ AlienSolomons/Assets/Scripts/Towers/SlowTower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTower : TowersDefault
+{
+    protected override void MyCollisions()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, TowerRadius.Value, EnemyLayerMask.Value);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            hitColliders[i].GetComponent<EnemyHealthManager>().HurtEnemy(TowerDamage.Value);
+            hitColliders[i].GetComponent<EnemyMove>().SetSlowDebuffTrue();
+            i++;
+
+        }
+    }
+}
---
+++ AlienSolomons/Assets/Scripts/Towers/TowersDefault.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowersDefault : MonoBehaviour
+{
+    private float _timer;
+    [SerializeField] protected LayerMaskVariable EnemyLayerMask;
+    [SerializeField] protected FloatVariable ActivateEveryX;
+    [SerializeField] protected FloatVariable TowerRadius;
+    [SerializeField] protected FloatVariable TowerDamage;
+
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= ActivateEveryX.Value)
+        {
+            MyCollisions();
+            _timer = 0;
+        }
+    }
+
+    protected virtual void MyCollisions()
+    {
+        Debug.Log("Select a specifc tower script");
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, TowerRadius.Value);
+    }
+}
