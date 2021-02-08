4dd4e54a39203a38b2cc6de0e045edf1a7d3498f
Justin <justinkatic4@gmail.com>
2021-02-09 09:46:13 +1100

    messing with archer inside proto 2

--- AJE/Assets/Scripts/Enemy AI/Archer/ArcherMove.cs
+++ AJE/Assets/Scripts/Enemy AI/Archer/ArcherMove.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class ArcherMove : EnemyMove
 {
     [SerializeField] FloatVariable MyAttackRange;
+    private ArcherShoot archerShoot;
+
+
+    private void Awake()
+    {
+        archerShoot = gameObject.GetComponent<ArcherShoot>();
+    }
 
     public override void Move()
     {
         float dist = Vector3.Distance(transform.position, targetDestination.position);
-        if (dist > MyAttackRange.RuntimeValue)
+
+        if (dist > MyAttackRange.RuntimeValue && archerShoot.shotTaken == true)
         {
+            archerShoot.CanShoot = false;
             navMeshAgent.isStopped = false;
             navMeshAgent.SetDestination(targetDestination.position);
         }
         else
         {
+
+            archerShoot.shotTaken = false;
+            archerShoot.CanShoot = true;
             navMeshAgent.isStopped = true;
             LookTowards();
         }
--- AJE/Assets/Scripts/Enemy AI/Archer/ArcherShoot.cs
+++ AJE/Assets/Scripts/Enemy AI/Archer/ArcherShoot.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class ArcherShoot : EnemyShoot
 {
+
+    public bool CanShoot;
+    public bool shotTaken;
+    [SerializeField] float lrDistance;
+    LineRenderer lr;
+
+
+    private void Awake()
+    {
+        shotTaken = true;
+        lr = gameObject.GetComponent<LineRenderer>();
+    }
     public override void Shoot()
     {
-        _shotCounter -= Time.deltaTime;
-        if (_shotCounter <= 0)
+        if (CanShoot)
         {
-            GameObject bullet = ObjectPooler.SharedInstance.GetPooledObject("ArcherProjectile");
-            _shotCounter = TimeBetweenShots.RuntimeValue;
-            bullet.transform.position = _firePoint.position;
-            bullet.transform.rotation = _firePoint.transform.rotation;
-            bullet.SetActive(true);
+            lr.positionCount = 2;
+            lr.SetPosition(0, gameObject.transform.position);
+            lr.SetPosition(1, gameObject.transform.position + gameObject.transform.forward * lrDistance);
+
+            _shotCounter -= Time.deltaTime;
+            if (_shotCounter <= 0)
+            {
+                GameObject bullet = ObjectPooler.SharedInstance.GetPooledObject("ArcherProjectile");
+                _shotCounter = TimeBetweenShots.RuntimeValue;
+                bullet.transform.position = _firePoint.position;
+                bullet.transform.rotation = _firePoint.transform.rotation;
+                bullet.SetActive(true);
+                shotTaken = true;
+                lr.positionCount = 0;
+            }
         }
     }
 }
