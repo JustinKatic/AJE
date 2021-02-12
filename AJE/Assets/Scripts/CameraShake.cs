e009352324d51440b587c2d3eec207e584f985b5
ElricM <elric.milton@yahoo.com.au>
2021-02-12 21:24:48 +1100

    Added Camera Shake

---
+++ AJE/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float duration = 0.11f;
+    public float magnitude = 0.15f;
+    public IEnumerator Shake (float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        StartCoroutine(Shake(duration, magnitude));
+    //    }
+    //}
+}
