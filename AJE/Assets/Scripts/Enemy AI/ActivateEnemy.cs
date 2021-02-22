35c2a9064932f4981e26609e20c48e730023c477
Justin <justinkatic4@gmail.com>
2021-02-22 14:02:19 +1100

    Auto stash before merge of "main" and "origin/main"

---
+++ AJE/Assets/Scripts/Enemy AI/ActivateEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateEnemy : MonoBehaviour
+{
+
+    [SerializeField] GameObject EnemyToSetActive;
+    [SerializeField] GameObject ParticleToSetUnActive;
+
+    [SerializeField] float timeUntilSpawn;
+
+    private void OnEnable()
+    {
+        StartCoroutine(WaitForSeconds());
+    }
+
+     IEnumerator WaitForSeconds()
+    {
+        yield return new WaitForSeconds(timeUntilSpawn);
+        EnemyToSetActive.SetActive(true);
+        ParticleToSetUnActive.SetActive(false);
+    }
+}
