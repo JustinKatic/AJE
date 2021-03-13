adb4f92fa0aca05cc63e6840c57f3de60f072242
Justin <justinkatic4@gmail.com>
2021-03-13 13:19:54 +1100

    created bools to set buttons true false

---
+++ AJE/Assets/Scripts/TowerButtonsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerButtonsController : MonoBehaviour
+{
+    [SerializeField] GameObject damageTowerButton;
+    [SerializeField] GameObject slowTowerButton;
+    [SerializeField] GameObject plagueTowerButton;
+    [SerializeField] GameObject vamparicTowerButton;
+
+
+    [SerializeField] BoolVariable isDmgButtonActive;
+    [SerializeField] BoolVariable isSlowButtonActive;
+    [SerializeField] BoolVariable isPlagueButtonActive;
+    [SerializeField] BoolVariable isVamparicButtonActive;
+
+
+    private void Start()
+    {
+        //DAMAGE TOWER BUTTON  set active/notActive
+        if (isDmgButtonActive.Value)
+            damageTowerButton.SetActive(true);
+        else
+            damageTowerButton.SetActive(false);
+
+        //SLOW TOWER BUTTON  set active/notActive
+        if (isSlowButtonActive.Value)
+            slowTowerButton.SetActive(true);
+        else
+            slowTowerButton.SetActive(false);
+
+        //PLAGUE TOWER BUTTON  set active/notActive
+        if (isPlagueButtonActive.Value)
+            plagueTowerButton.SetActive(true);
+        else
+            plagueTowerButton.SetActive(false);
+
+        //VAMPARIC TOWER BUTTON  set active/notActive
+        if (isVamparicButtonActive.Value)
+            vamparicTowerButton.SetActive(true);
+        else
+            vamparicTowerButton.SetActive(false);
+
+
+    }
+}
