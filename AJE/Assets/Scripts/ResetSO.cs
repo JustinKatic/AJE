fb8b0df30d7318f61a6edaee177240bef56b9acd
Justin <justinkatic4@gmail.com>
2021-03-13 12:46:04 +1100

    fixed bug levels not reseting correct after menus

--- AJE/Assets/Scripts/ResetSO.cs
+++ AJE/Assets/Scripts/ResetSO.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ResetSO : MonoBehaviour
 {
     [SerializeField] FloatVariable playerInGameCurrency;
+    [SerializeField] FloatVariable ActiveEnemies;
+
     [SerializeField] IntVariable nextWave;
 
     [SerializeField] GameEvent UpdateCurrency;
 
     private void OnEnable()
     {
+        ActiveEnemies.RuntimeValue = ActiveEnemies.Value;
         nextWave.RuntimeValue = nextWave.Value;
         playerInGameCurrency.RuntimeValue = playerInGameCurrency.Value;
         UpdateCurrency.Raise();
