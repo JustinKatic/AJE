049238b026b8652e21a55f4fe2f08f0b926805fb
Justin <justinkatic4@gmail.com>
2021-03-16 11:37:20 +1100

    working on menus ;(

---
+++ AJE/Assets/Prefabs/TowerMenuUI/SetTowerActive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTowerActive : MonoBehaviour
+{
+    [SerializeField] BoolVariable myTowerActiveState;
+
+    private void OnEnable()
+    {
+        myTowerActiveState.Value = true;
+    }
+
+    private void OnDisable()
+    {
+        myTowerActiveState.Value = false;
+    }
+}
