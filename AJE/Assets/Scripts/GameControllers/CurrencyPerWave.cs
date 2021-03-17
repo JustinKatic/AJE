7da24b01f4de18ec818cbfdadf2b8d9ff6ed90eb
Justin <justinkatic4@gmail.com>
2021-03-17 14:04:50 +1100

    new currency gained method added

---
+++ AJE/Assets/Scripts/GameControllers/CurrencyPerWave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyPerWave : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public int value;
+    }
+
+    [SerializeField] IntVariable nextWaveNum;
+    [SerializeField] Wave[] wave;
+
+
+    [SerializeField] FloatVariable PlayerInGameCurrency;
+    [SerializeField] GameEvent UpdateCurrencyTextEvent;
+
+    public void AddCurrency()
+    {
+        PlayerInGameCurrency.RuntimeValue += wave[nextWaveNum.RuntimeValue - 1].value;
+        UpdateCurrencyTextEvent.Raise();
+    }
+}
