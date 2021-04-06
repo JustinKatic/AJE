4f6939653f056f626d31c0357274ba41825b9acb
ElricM <elric.milton@yahoo.com.au>
2021-04-06 11:07:48 +1000

    Animated currency update

--- AJE/Assets/Scripts/UpdateUIFloat.cs
+++ AJE/Assets/Scripts/UpdateUIFloat.cs
@@ -9,14 +9,17 @@ public class UpdateUIFloat : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI TextToUpdate;
     [SerializeField] FloatVariable FloatNeedingUpdated;
+    Animator anim;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
         UpdateText(FloatNeedingUpdated);
     }
 
     public void UpdateText(FloatVariable floatVariable)
     {
+        anim.Play("CurrencyTextScale");
         TextToUpdate.text = floatVariable.RuntimeValue.ToString();
     }
 }
