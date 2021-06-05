c69fd88a03b4707bd972e442783f989e6079b54a
ElricM <elric.milton@yahoo.com.au>
2021-06-05 17:29:46 +1000

    Fixed all unbuildable squares across all levels

--- JEAH!/Assets/Scripts/Towers/SetAreaUnbuildable.cs
+++ JEAH!/Assets/Scripts/Towers/SetAreaUnbuildable.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class SetAreaUnbuildable : MonoBehaviour
 {
     public bool _hasAreaBeenBuiltOn = false;
+    private void OnDrawGizmos()
+    {
+        if (_hasAreaBeenBuiltOn)
+            Gizmos.DrawCube(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Vector3.one);
+    }
 }
