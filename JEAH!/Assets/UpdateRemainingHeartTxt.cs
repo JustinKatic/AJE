185f000285baea3fb94ad8b0638d2ed19c7145c5
Justin <justinkatic4@gmail.com>
2021-06-07 21:07:55 +1000

    change update hearts txt to value instead of runtime val

--- JEAH!/Assets/UpdateRemainingHeartTxt.cs
+++ JEAH!/Assets/UpdateRemainingHeartTxt.cs
@@ -9,6 +9,6 @@ public class UpdateRemainingHeartTxt : MonoBehaviour
     public IntVariable hearts;
     private void OnEnable()
     {
-        heartText.text = hearts.RuntimeValue.ToString();
+        heartText.text = hearts.Value.ToString();
     }
 }
